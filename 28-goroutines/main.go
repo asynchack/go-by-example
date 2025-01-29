e81e5c3ed3e630fd1128f6b3b078a8f34f180a7d
asynchack <asynchack@gmail.com>
2025-01-29 17:58:19 +0800

    learn: goroutines

---
+++ 28-goroutines/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := range 3 {
+		fmt.Println(from, ":", i)
+	}
+}
+func main() {
+	f("direct")
+
+	go f("goroutine")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("anony")
+
+	time.Sleep(time.Second)
+	fmt.Println("done")
+}
